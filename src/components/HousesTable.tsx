ea9c4e7fe3fa29848b6a96f5a921f78fcb62e525
g34r21 <gear21@icloud.com>
2024-08-28 23:17:21 -0500

    refactor: remove data grid

--- src/components/HousesTable.tsx
+++ src/components/HousesTable.tsx
@@ -1,26 +1,54 @@
 'use client';
 
-import { Button, Typography } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import {
+  Button,
+  Pagination,
+  Paper,
+  Skeleton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from '@mui/material';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 
 import useGetHouses from '@/hooks/useGetHouses';
 
+const HousesSkeleton = () => {
+  return (
+    <>
+      {Array.from(Array(12)).map((_, i) => (
+        <TableRow
+          // eslint-disable-next-line react/no-duplicate-key, react/no-array-index-key
+          key={i}
+          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        >
+          <TableCell component="th" scope="row">
+            <Skeleton />
+          </TableCell>
+          <TableCell align="right">
+            <Skeleton />
+          </TableCell>
+        </TableRow>
+      ))}
+    </>
+  );
+};
+
 const Houses = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const currentPage = Number(searchParams.get('page')) || 1;
-  const handlePaginationModelChange = ({
-    page,
-  }: {
-    page: number;
-    pageSize: number;
-  }) => {
+  const handlePaginationModelChange = (
+    _: React.ChangeEvent<unknown>,
+    page: number,
+  ) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page', (page + 1).toString());
+    params.set('page', page.toString());
     replace(`${pathname}?${params.toString()}`);
   };
   const { data, isLoading, error } = useGetHouses({
@@ -32,60 +60,41 @@ const Houses = () => {
   }
 
   return (
-    <DataGrid
-      rows={data?.pageResults.map(house => ({
-        id: house.id,
-        name: house.name,
-        link: `/houses/${house.id}/members`,
-      }))}
-      disableRowSelectionOnClick
-      disableColumnSelector
-      disableColumnSorting
-      disableColumnFilter
-      disableColumnResize
-      disableColumnMenu
-      columns={[
-        {
-          field: 'name',
-          renderHeader: () => (
-            <Typography variant="h6" fontWeight="bold">
-              House name
-            </Typography>
-          ),
-          flex: 3,
-        },
-        {
-          field: 'link',
-          flex: 1,
-          headerName: '',
-          align: 'right',
-          renderCell: ({ value }) => (
-            <Link href={value} role="link">
-              <Button variant="contained" size="medium">
-                members
-              </Button>
-            </Link>
-          ),
-        },
-      ]}
-      paginationMode="server"
-      rowCount={data?.totalCount || 10}
-      pagination
-      initialState={{
-        pagination: {
-          paginationModel: { pageSize: 10, page: currentPage - 1 },
-        },
-      }}
-      loading={isLoading}
-      pageSizeOptions={[10]}
-      slotProps={{
-        loadingOverlay: {
-          variant: 'skeleton',
-          noRowsVariant: 'skeleton',
-        },
-      }}
-      onPaginationModelChange={handlePaginationModelChange}
-    />
+    <>
+      <TableContainer component={Paper} sx={{ marginY: 4 }}>
+        <Table>
+          <TableBody>
+            {isLoading && <HousesSkeleton />}
+            {!isLoading
+            && data?.pageResults.map(house => (
+              <TableRow
+                // eslint-disable-next-line react/no-duplicate-key
+                key={house.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {house.name}
+                </TableCell>
+                <TableCell align="right">
+                  <Link href={`/houses/${house.id}/members`} role="link">
+                    <Button variant="contained" size="medium">
+                      members
+                    </Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+        color="primary"
+        count={data?.totalCount ? data?.totalCount / 10 : 1}
+        page={currentPage}
+        onChange={handlePaginationModelChange}
+      />
+    </>
   );
 };
 
